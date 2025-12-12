cc90b96ca84f053234e1f949bfb528c3a4adc7e5
Vlad Meșco <alzwded@gmail.com>
2025-12-12 15:02:12 +0200

    extend worddb queries to multiple words

--- .vim/plugin/worddb.vim
+++ .vim/plugin/worddb.vim
@@ -40,7 +40,11 @@ function! IworddbInBuffer(args)
         call inputrestore()
         return
     endif
-    let cmdline = s:path . '/worddb_query.sh ' . a:args . ' "' . searchfor . '"'
+    if a:args != 'f' && stridx(searchfor, " ") > -1
+        let cmdline = s:path . '/worddb_query.sh ' . a:args . 'n ' . searchfor
+    else
+        let cmdline = s:path . '/worddb_query.sh ' . a:args . ' "' . searchfor . '"'
+    endif
     " run query
     let choices = systemlist(cmdline)
     :execute "normal :new\<CR>"
@@ -57,14 +61,18 @@ function! Iworddbfiles(args)
         call inputrestore()
         return
     endif
-    let cmdline = s:path . '/worddb_query.sh ' . a:args . ' "' . searchfor . '"'
+    if a:args != 'f' && stridx(searchfor, " ") > -1
+        let cmdline = s:path . '/worddb_query.sh ' . a:args . 'n ' . searchfor
+    else
+        let cmdline = s:path . '/worddb_query.sh ' . a:args . ' "' . searchfor . '"'
+    endif
     " run query
     let choices = systemlist(cmdline)[0:199]
     " add first prompt to choices
     let choices = ['Results:'] + choices
     let i = 1
     while i < len(choices)
-        let choices[i] = printf("%4d ", i) . choices[i]
+        let choices[i] = printf("%4d#", i) . choices[i]
         let i = i + 1
     endwhile
     if len(choices) == 201
@@ -74,9 +82,19 @@ function! Iworddbfiles(args)
     let n = inputlist(choices)
     if n > 0 && n < len(choices) && n < 201
         let chosen = choices[n]
-        let chunks = split(chosen, '\s\+')
-        let fname = chunks[1]
-        :execute 'e ' . fname
+        " strip out choice number
+        let spc = stridx(chosen, '#')
+        let chosen = chosen[spc+1:]
+        " figure out if we ran grep -Hn or we just have a file list
+        let chunks = split(chosen, ':')
+        if len(chunks) > 1
+            let fname = chunks[0]
+            let lno = chunks[1]
+            :execute 'e +' . lno . ' ' . fname
+        else
+            let fname = chosen
+            :execute 'e ' . fname
+        endif
     endif
     call inputrestore()
 endfunction
@@ -94,7 +112,7 @@ function! WorddbOpenFileFromLineRefInNew()
     endif
 endfunction
 
-:map <C-L>we <ESC>:call Iworddb()<CR>
+:map <C-L>we <ESC>:call Iworddbfiles('e')<CR>
 :map <C-L>wE <ESC>:call IworddbInBuffer('e')<CR>
 :map <C-L>ww <ESC>:call Iworddbfiles('w')<CR>
 :map <C-L>wf <ESC>:call Iworddbfiles('f')<CR>
--- .vim/plugin/worddb_query.sh
+++ .vim/plugin/worddb_query.sh
@@ -3,39 +3,84 @@
 DB="${WORDDBPATH:-worddb.sqlite3}"
 
 showhelp() {
-    echo "Usage: $0 w|e|f word"
+    echo "Usage: $0 w|e|f globpattern"
+    echo "       $0 wn|en words..."
     echo ""
-    echo "    w         print files containing this word"
-    echo "    e         print lines containing this word"
-    echo "    f         print files whose path matches word"
+    echo "    w         print files containing this pattern"
+    echo "    e         print lines containing this pattern"
+    echo "    f         print files whose path matches pattern"
+    echo "    wn        print files containing multiple words"
+    echo "    we        print lines from files containing selected words"
     echo ""
-    echo "Glob patterns (*,?) are accepted"
+    echo "The e variants prints "file:linenumber: line text"
+    echo "The other variants print file names
     exit 2
 }
 
 THING="$2"
 GREPTHING="$2"
 
-THING="${THING//_/@_}"
-THING="${THING//\*/%}"
-THING="${THING//\?/_}"
-
 GREPTHING="${GREPTHING//\*/.*}"
 GREPTHING="${GREPTHING//\?/.?}"
 
+select_multiple() {
+    if [[ $# -lt 1 ]] ; then
+        return
+    fi
+
+    local WORDLIST="'$1'"
+    local N=$#
+    local word
+    shift
+    for word in "$@" ; do
+        WORDLIST="$WORDLIST , '$word'"
+    done
+
+    # Note to self, for wordids, you can say word glob '$1' [ or word glob '$2' ... ],
+    # then check with wordcount>$N or some better expression;
+    # To properly select multiple glob matches, we'd probably need separate
+    # tables and make sure a fileid is in ALL tables, which is starting to get
+    # quite excessive. There's probably some SQL magic sugar I'm not aware of.
+    local Q="WITH
+                  wordids AS (SELECT id FROM words WHERE word IN ($WORDLIST)),
+                  filteredrefs AS (SELECT * FROM refs WHERE word IN wordids),
+                  counts(fileid, wordcount) AS (SELECT file, COUNT(word) FROM filteredrefs GROUP BY file),
+                  selectedfiles(id) AS (SELECT fileid FROM counts WHERE wordcount=$N)
+             SELECT file FROM files WHERE id IN selectedfiles;"
+
+    #echo "$Q"
+
+    sqlite3 "$DB" "$Q"
+}
+
 case $1 in
-    *h) showhelp ;;
     e)
-        IFS='
-'
-        for F in $(sqlite3 "$DB" "select file from files where id in (select file from refs where word in ( select id from words where word like '$THING' escape '@' ))" ) ; do
+        for F in $(IFS='
+' sqlite3 "$DB" "select file from files where id in (select file from refs where word in ( select id from words where word glob '$THING' ))" ) ; do
             grep -Hn "$GREPTHING" "$F"
         done
         ;;
     w)
-        sqlite3 "$DB" "select file from files where id in (select file from refs where word in ( select id from words where word like '$THING' escape '@' ))"
+        sqlite3 "$DB" "select file from files where id in (select file from refs where word in ( select id from words where word glob '$THING' ))"
+        ;;
+    wn)
+        # strip wn
+        shift
+        select_multiple "$@"
+        ;;
+    en)
+        # strip wn
+        shift
+        printf -v POLYGREP '%s\|' "$@"
+        for f in $(IFS='
+' select_multiple "$@"); do
+            #echo "${POLYGREP%\\|}" "$f"
+            grep -Hn -e "${POLYGREP%\\|}" "$f"
+        done
         ;;
     f)
-        sqlite3 "$DB" 'select distinct file from files where file like "%'"$THING"'%" escape "@";'
+        sqlite3 "$DB" 'select distinct file from files where file glob "*'"$THING"'*";'
         ;;
+    *h) showhelp ;;
+    *) showhelp ;;
 esac
